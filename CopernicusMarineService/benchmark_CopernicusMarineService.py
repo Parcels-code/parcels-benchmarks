31be70c04840c616cb360776cb982c2e355acbef
Erik van Sebille <erikvansebille@gmail.com>
2025-07-23 14:48:39 +0200

    Adding correctness test to CopernicusMarine benchmark

--- CopernicusMarineService/benchmark_CopernicusMarineService.py
+++ CopernicusMarineService/benchmark_CopernicusMarineService.py
@@ -80,6 +80,7 @@ def run_benchmark(interpolator: str):
 
     files = f"{DATA_ROOT}/SMOC_202101*.nc"
 
+    lon0_expected, lat0_expected = -9.820091, -30.106716  # values from v3
     if parcelsv4:
         if interpolator == "BiRectiLinear":
             interp_method = BiRectiLinear
@@ -87,9 +88,11 @@ def run_benchmark(interpolator: str):
             interp_method = PureXarrayInterp
         elif interpolator == "NoFieldAccess":
             interp_method = NoFieldAccess
+            lon0_expected, lat0_expected = -10, -30  # Zero interpolation, so expect initial values
         else:
             raise ValueError(f"Unknown interpolator: {interpolator}")
         ds = xr.open_mfdataset(files, concat_dim="time", combine="nested", data_vars='minimal', coords='minimal', compat='override')
+        ds = ds.drop_vars(["vsdx", "vsdy", "utide", "vtide", "utotal", "vtotal"])
         ds = ds.rename({"uo": "U", "vo": "V", "longitude": "lon", "latitude": "lat"})
 
         xgcm_grid = parcels.xgcm.Grid(ds, coords={"X": {"left": "lon"}, "Y": {"left": "lat"}, "Z": {"left": "depth"}, "T": {"center": "time"}})
@@ -110,7 +113,7 @@ def run_benchmark(interpolator: str):
 
     kernel = parcels.AdvectionEE
 
-    for npart in [1, 10, 100, 1000, 10000]:
+    for npart in [1, 10, 100, 1000, 5000, 10000]:
         lon = np.linspace(-10, 10, npart)
         lat = np.linspace(-30, -20, npart)
 
@@ -118,6 +121,9 @@ def run_benchmark(interpolator: str):
 
         print(f"Running {len(lon)} particles with parcels v{4 if parcelsv4 else 3} and {interpolator} interpolator")
         pset.execute(kernel, runtime=runtime, dt=dt)
+
+        assert np.allclose(pset[0].lon, lon0_expected, atol=1e-5), f"Expected lon {lon0_expected}, got {pset[0].lon}"
+        assert np.allclose(pset[0].lat, lat0_expected, atol=1e-5), f"Expected lat {lat0_expected}, got {pset[0].lat}"
 
 
 def main(args=None):
