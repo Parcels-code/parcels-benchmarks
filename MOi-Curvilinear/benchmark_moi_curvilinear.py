b013cef260bd315d69a3ea4896fbbcb60c890429
Erik van Sebille <erikvansebille@gmail.com>
2025-08-13 10:11:49 +0200

    Updating moi curvilinear benchmark to also assess peak memory

--- MOi-Curvilinear/benchmark_moi_curvilinear.py
+++ MOi-Curvilinear/benchmark_moi_curvilinear.py
@@ -1,6 +1,8 @@
 from argparse import ArgumentParser
 from pathlib import Path
 import xarray as xr
+import tracemalloc
+import time
 
 from glob import glob
 
@@ -10,74 +12,17 @@ import parcels
 
 runtime = np.timedelta64(2, "D")
 dt = np.timedelta64(15, "m")
-depth_range = range(0, 2)  # only taking upper-two depth levels
 
 parcelsv4 = True
 try:
     from parcels.xgrid import _XGRID_AXES
+    from parcels.application_kernels.interpolation import XLinear
 except ImportError:
     parcelsv4 = False
 
 DATA_ROOT = "/storage/shared/oceanparcels/input_data/MOi"
 
-def run_benchmark(interpolator: str):
-    if parcelsv4:
-
-        def BiRectiLinear(
-            field: parcels.Field,
-            ti: int,
-            position: dict[_XGRID_AXES, tuple[int, float | np.ndarray]],
-            tau: np.float32 | np.float64,
-            t: np.float32 | np.float64,
-            z: np.float32 | np.float64,
-            y: np.float32 | np.float64,
-            x: np.float32 | np.float64,
-        ):
-            """Bilinear interpolation on a rectilinear grid."""
-            xi, xsi = position["X"]
-            yi, eta = position["Y"]
-
-            data = field.data.data[:, :, yi:yi + 2, xi:xi + 2]
-            val_t0 =(
-                (1 - xsi) * (1 - eta) * data[0, 0, 0, 0]
-                + xsi * (1 - eta) * data[0, 0, 0, 1]
-                + xsi * eta * data[0, 0, 1, 1]
-                + (1 - xsi) * eta * data[0, 0, 1, 0]
-            )
-
-            val_t1 =(
-                (1 - xsi) * (1 - eta) * data[1, 0, 0, 0]
-                + xsi * (1 - eta) * data[1, 0, 0, 1]
-                + xsi * eta * data[1, 0, 1, 1]
-                + (1 - xsi) * eta * data[1, 0, 1, 0]
-            )
-            return (val_t0 * (1 - tau) + val_t1 * tau)
-
-        def PureXarrayInterp(
-            field: parcels.Field,
-            ti: int,
-            position: dict[_XGRID_AXES, tuple[int, float | np.ndarray]],
-            tau: np.float32 | np.float64,
-            t: np.float32 | np.float64,
-            z: np.float32 | np.float64,
-            y: np.float32 | np.float64,
-            x: np.float32 | np.float64,
-        ):
-            return field.data.interp(time=t, lon=x, lat=y).values[0]
-
-
-        def NoFieldAccess(
-            field: parcels.Field,
-            ti: int,
-            position: dict[_XGRID_AXES, tuple[int, float | np.ndarray]],
-            tau: np.float32 | np.float64,
-            t: np.float32 | np.float64,
-            z: np.float32 | np.float64,
-            y: np.float32 | np.float64,
-            x: np.float32 | np.float64,
-        ):
-            return 0
-
+def run_benchmark(interpolator: str, trace_memory: bool = False):
 
     lon0_expected, lat0_expected = -10.128929, -29.721205  # values from v3 using from_netcf (so assuming A-grid!)
 
@@ -86,13 +31,8 @@ def run_benchmark(interpolator: str):
     mesh_mask = f"{DATA_ROOT}/domain_ORCA0083-N006/PSY4V3R1_mesh_hgr.nc"
 
     if parcelsv4:
-        if interpolator == "BiRectiLinear":
-            interp_method = BiRectiLinear
-        elif interpolator == "PureXarrayInterp":
-            interp_method = PureXarrayInterp
-        elif interpolator == "NoFieldAccess":
-            interp_method = NoFieldAccess
-            lon0_expected, lat0_expected = -10, -30  # Zero interpolation, so expect initial values
+        if interpolator == "XLinear":
+            interp_method = XLinear
         else:
             raise ValueError(f"Unknown interpolator: {interpolator}")
 
@@ -106,7 +46,6 @@ def run_benchmark(interpolator: str):
         ds_mesh = xr.open_dataset(mesh_mask)[["glamf", "gphif"]].isel(t=0)
 
         ds = xr.merge([ds_u, ds_v, ds_depth, ds_mesh], compat="identical").rename({"vozocrtx": "U", "vomecrty": "V"}).rename({"glamf": "lon", "gphif": "lat", "time_counter": "time", "depthw": "depth"})
-        ds = ds.isel(depth=depth_range)
 
         xgcm_grid = parcels.xgcm.Grid(
             ds,
@@ -116,6 +55,7 @@ def run_benchmark(interpolator: str):
                 "Z": {"center": "deptht", "left": "depth"},
                 "T": {"center": "time"},
             },
+            periodic=False,
         )
         grid = parcels.xgrid.XGrid(xgcm_grid)
 
@@ -136,19 +76,34 @@ def run_benchmark(interpolator: str):
             filenames,
             variables={"U": "vozocrtx", "V": "vomecrty"},
             dimensions={"time": "time_counter", "lat": "gphif", "lon": "glamf", "depth": "depthw"},
-            indices={"depth": depth_range},
         )
 
     pclass = parcels.Particle if parcelsv4 else parcels.JITParticle
 
-    for npart in [1, 10, 100, 1000, 5000, 10000]:
+    for npart in [1, 10, 100, 1_000, 5_000, 10_000, 50_000, 100_000, 500_000, 1_000_000]:
         lon = np.linspace(-10, 10, npart)
         lat = np.linspace(-30, -20, npart)
 
         pset = parcels.ParticleSet(fieldset=fieldset, pclass=pclass, lon=lon, lat=lat)
 
         print(f"Running {len(lon)} particles with parcels v{4 if parcelsv4 else 3} and {interpolator} interpolator")
-        pset.execute(parcels.AdvectionEE, runtime=runtime, dt=dt)
+
+        if trace_memory:
+            tracemalloc.start()
+        else:
+            start = time.time()
+
+        pset.execute(parcels.AdvectionEE, runtime=runtime, dt=dt, verbose_progress=False)
+
+        if trace_memory:
+            current, peak = tracemalloc.get_traced_memory()
+            tracemalloc.stop()
+            print(f"Memory usage: current={current / 1e6:.0f} MB, peak={peak / 1e6:.0f} MB")
+        else:
+            elapsed_time = time.time() - start
+            print(f"Execution time: {elapsed_time:.2f} seconds")
+
+        print("")
 
         assert np.allclose(pset[0].lon, lon0_expected, atol=1e-5), f"Expected lon {lon0_expected}, got {pset[0].lon}"
         assert np.allclose(pset[0].lat, lat0_expected, atol=1e-5), f"Expected lat {lat0_expected}, got {pset[0].lat}"
@@ -160,12 +115,19 @@ def main(args=None):
     p.add_argument(
         "-i",
         "--Interpolator",
-        choices=("BiRectiLinear", "PureXarrayInterp", "NoFieldAccess"),
-        default="BiRectiLinear",
+        choices=("XLinear", "BiRectiLinear", "PureXarrayInterp", "NoFieldAccess"),
+        default="XLinear",
+    )
+
+    p.add_argument(
+        "-m",
+        "--memory",
+        action="store_true",
+        help="Enable memory tracing (default: False)",
     )
 
     args = p.parse_args(args)
-    run_benchmark(args.Interpolator)
+    run_benchmark(args.Interpolator, args.memory)
 
 
 if __name__ == "__main__":
