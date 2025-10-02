43c4575ec71cd7ffe7dd7a582ebd582dfa9a43c7
Willi Rath <wrath@geomar.de>
2025-10-02 14:22:33 +0200

    Add hash for MOi

--- benchmarks/benchmark_moi_curvilinear.py
+++ benchmarks/benchmark_moi_curvilinear.py
@@ -18,7 +18,7 @@ from utils import retrieve_data_dir
 
 DATA_ROOT = retrieve_data_dir(
     url="https://surfdrive.surf.nl/index.php/s/7xlfdOFaUGDEmpD/download?path=%2F&files=Parcels_Benchmarks_MOi_data.zip",
-    known_hash=None
+    known_hash="f7816d872897c089eeb07a4e32b7fbcc96a0023ef01ac6c3792f88d8d8893885",
 )
 
 
