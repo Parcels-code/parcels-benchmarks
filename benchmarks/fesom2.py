c2eb313c51837ec4dd854f5304a1131ea0dd861a
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2026-03-27 16:55:59 +0100

    Migrate fesom ingestion to intake

--- benchmarks/fesom2.py
+++ benchmarks/fesom2.py
@@ -7,40 +7,36 @@ from parcels import (
     ParticleSet,
     convert,
 )
+import xarray as xr
 from parcels.kernels import AdvectionRK2_3D
-from parcels_benchmarks.benchmark_setup import PARCELS_DATADIR, download_dataset
+from . import PARCELS_BENCHMARKS_DATA_FOLDER
 
 runtime = np.timedelta64(1, "D")
 dt = np.timedelta64(2400, "s")
 
 
-def _load_ds(datapath):
+def _load_ds():
     """Helper function to load uxarray dataset from datapath"""
 
-    grid_file = f"{datapath}/mesh/fesom.mesh.diag.nc"
-    data_files = f"{datapath}/*.nc"
-    return ux.open_mfdataset(grid_file, data_files, combine="by_coords")
+    grid_file = xr.open_mfdataset(f"{PARCELS_BENCHMARKS_DATA_FOLDER}/surf-data/parcels-benchmarks/data/Parcelsv4_Benchmarking_data/Parcels_Benchmarks_FESOM-baroclinic-gyre/data/mesh/fesom.mesh.diag.nc")
+    data_files = xr.open_mfdataset(f"{PARCELS_BENCHMARKS_DATA_FOLDER}/surf-data/parcels-benchmarks/data/Parcelsv4_Benchmarking_data/Parcels_Benchmarks_FESOM-baroclinic-gyre/data/*.nc")
+
+    grid = ux.open_grid(grid_file)
+    return ux.UxDataset(data_files, uxgrid = grid)
 
 
 class FESOM2:
     params = ([10000], [AdvectionRK2_3D])
     param_names = ["npart", "integrator"]
 
-    def setup(self, npart, integrator):
-        # Ensure the dataset is downloaded in the desired data_home
-        # and obtain the path to the dataset
-        self.datapath = download_dataset(
-            "FESOM-baroclinic-gyre", data_home=PARCELS_DATADIR
-        )
-
     def time_load_data(self, npart, integrator):
-        ds = _load_ds(self.datapath)
+        ds = _load_ds()
         for i in range(min(ds.coords["time"].size, 2)):
             _u = ds["u"].isel(time=i).compute()
             _v = ds["v"].isel(time=i).compute()
 
     def pset_execute(self, npart, integrator):
-        ds = _load_ds(self.datapath)
+        ds = _load_ds()
         ds = convert.fesom_to_ugrid(ds)
         fieldset = FieldSet.from_ugrid_conventions(ds)
 
