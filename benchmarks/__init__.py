0c2b6cf8b975ddeabacf5a170332552e88b7f233
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2026-03-27 16:55:15 +0100

    Fix catalogues

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -2,10 +2,16 @@ import os
 from pathlib import Path
 import intake
 
+PARCELS_BENCHMARKS_DATA_FOLDER: str
+PROJECT_ROOT = Path(__file__).parent.parent.resolve()
+
 try:
     PARCELS_BENCHMARKS_DATA_FOLDER = Path(os.environ["PARCELS_BENCHMARKS_DATA_FOLDER"])
 except KeyError as e:
     raise RuntimeError("Set the PARCELS_BENCHMARKS_DATA_FOLDER environment variable to specify where the data is/should be downloaded.") from e
 
-CAT_EXAMPLES = intake.open_catalog('surf-data/parcels-examples/catalog.yml')
-CAT_BENCHMARKS = intake.open_catalog('surf-data/parcels-benchmarks/catalog.yml')
+if not PARCELS_BENCHMARKS_DATA_FOLDER.is_absolute():
+    PARCELS_BENCHMARKS_DATA_FOLDER = PROJECT_ROOT / str(PARCELS_BENCHMARKS_DATA_FOLDER)
+
+CAT_EXAMPLES = intake.open_catalog(f'{PARCELS_BENCHMARKS_DATA_FOLDER}/surf-data/parcels-examples/catalog.yml')
+CAT_BENCHMARKS = intake.open_catalog(f'{PARCELS_BENCHMARKS_DATA_FOLDER}/surf-data/parcels-benchmarks/catalog.yml')
