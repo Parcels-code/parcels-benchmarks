4c16edecdfcaf54b20a8381f3e80fc7c442960a8
Wyatt Sieminski <wyatt@fluidnumerics.com>
2026-09-02 10:33:26 -0400

    Compatability updates for parcelsv4 (#50)
    * Changed pixi verion requirements to match parcelsv4
    
    * Pinned ./Parcels to the most up to date version of parcels v4
    
    * updated pixi.lock to new ./Parcels submodule
    
    * Add delete_error_particles kernel and to_windowed_array to fesom benchmarking
    
    * Add delete_error_particles kernel, to_windowed_array, and resolve parcelsv4 compatability in moi benchmarking
    
    * Updates to results/benchamrks.json
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Avoid .to_dask float32->flaot64 conversion and remove interpolator from moi benchmarks
    
    * Reran with new moi benchmark and parcels windowed array cache change
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- benchmarks/moi_curvilinear.py
+++ benchmarks/moi_curvilinear.py
@@ -1,11 +1,16 @@
 import numpy as np
 import parcels
-from parcels.interpolators import XLinear
 
 from .catalogs import Catalogs
 
 runtime = np.timedelta64(2, "D")
 dt = np.timedelta64(15, "m")
+
+
+def _delete_error_particles(particles, fieldset):
+    """Delete any particle that errors out (e.g. runs aground or off the grid)"""
+    any_error = particles.state >= 50
+    particles[any_error].state = parcels.StatusCode.Delete
 
 
 def _load_ds(chunk):
@@ -19,6 +24,11 @@ def _load_ds(chunk):
     ds_v = (
         cat.moi_v(chunks=chunks).to_dask()[["vomecrty"]].rename_vars({"vomecrty": "V"})
     )
+
+    # undo the .to_dask float32 -> float64 conversion
+    ds_u["U"] = ds_u["U"].astype("float32")
+    ds_v["V"] = ds_v["V"].astype("float32")
+
     da_depth = cat.moi_w(chunks=chunks).to_dask()["depthw"]
     ds_mesh = cat.moi_mesh(chunks=None).read()[["glamf", "gphif"]].isel(t=0)
     ds_mesh["depthw"] = da_depth
@@ -31,49 +41,45 @@ class MOICurvilinear:
     """Mercator Ocean International model data based benchmark on a curvilinear grid."""
 
     params = (
-        ["XLinear"],
         [256],
         [10000],
     )
     param_names = [
-        "interpolator",
         "chunk",
         "npart",
     ]
 
-    def time_load_data_3d(self, interpolator, chunk, npart):
+    def time_load_data_3d(self, chunk, npart):
         """Benchmark that times loading the 'U' and 'V' data arrays only for 3-D"""
 
         # To have a reasonable runtime, we only consider the time it takes to load two time levels
         # and two depth levels (at most)
         ds = _load_ds(chunk)
-        for j in range(min(ds.coords["deptht"].size, 2)):
+        for j in range(min(ds.coords["depth_center"].size, 2)):
             for i in range(min(ds.coords["time"].size, 2)):
-                _u = ds["U"].isel(deptht=j, time=i).compute()
-                _v = ds["V"].isel(deptht=j, time=i).compute()
+                _u = ds["U"].isel(depth_center=j, time=i).compute()
+                _v = ds["V"].isel(depth_center=j, time=i).compute()
 
-    def pset_execute_3d(self, interpolator, chunk, npart):
+    def pset_execute_3d(self, chunk, npart):
         ds = _load_ds(chunk)
         fieldset = parcels.FieldSet.from_sgrid_conventions(ds)
-        if interpolator == "XLinear":
-            fieldset.U.interp_method = XLinear
-            fieldset.V.interp_method = XLinear
-        else:
-            raise ValueError(f"Unknown interpolator: {interpolator}")
-
+        fieldset = fieldset.to_windowed_arrays()
         pclass = parcels.Particle
 
         lon = np.linspace(-10, 10, npart)
         lat = np.linspace(-30, -20, npart)
 
-        pset = parcels.ParticleSet(fieldset=fieldset, pclass=pclass, lon=lon, lat=lat)
+        pset = parcels.ParticleSet(fieldset=fieldset, pclass=pclass, x=lon, y=lat)
 
         pset.execute(
-            parcels.kernels.AdvectionEE, runtime=runtime, dt=dt, verbose_progress=False
+            [parcels.kernels.AdvectionEE, _delete_error_particles],
+            runtime=runtime,
+            dt=dt,
+            verbose_progress=False,
         )
 
-    def time_pset_execute_3d(self, interpolator, chunk, npart):
-        self.pset_execute_3d(interpolator, chunk, npart)
+    def time_pset_execute_3d(self, chunk, npart):
+        self.pset_execute_3d(chunk, npart)
 
-    def peakmem_pset_execute_3d(self, interpolator, chunk, npart):
-        self.pset_execute_3d(interpolator, chunk, npart)
+    def peakmem_pset_execute_3d(self, chunk, npart):
+        self.pset_execute_3d(chunk, npart)
